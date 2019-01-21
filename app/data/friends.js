12fd850e1de79f26a998f9ff89386ac4e679b8f0
min <mjnorthwesternwork1121@gmail.com>
2019-01-21 03:55:03 -0600

    cleaning up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        name: "Ross Geller",
-        image: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
-        scores: [3, 4, 2, 1, 4, 5, 3, 4, 5, 2],
-    },
-
-    {
         name: "Phoebe Buffay",
         image: "http://25.media.tumblr.com/23e479224f5179ac42665e7a648cf309/tumblr_mndv0cYih81r9zaago1_500.gif",
         scores: [5, 2, 3, 3, 5, 2, 1, 4, 5, 2],
+    },
+
+    {
+        name: "Ross Geller",
+        image: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
+        scores: [3, 4, 2, 1, 4, 5, 3, 4, 5, 2],
     },
     {
         name: "Rachel Green",
@@ -16,7 +16,7 @@ module.exports = [
         scores: [2, 4, 5, 2, 3, 4, 5, 3, 2, 5],
     },
     {
-        name: "Sponge-Bob",
+        name: "Sponge-Bob Squarepants",
         image: "https://media.giphy.com/media/3ohuPpJ3YIQmcOVyJW/giphy.gif",
         scores: [4, 3, 4, 1, 4, 5, 2, 1, 2, 3],
     },
